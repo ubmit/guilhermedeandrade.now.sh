3785855299904e42fd802b608f7bb5ded3105b80
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-09-25 20:39:57 +0100

    /blog is now showing a coming soon message

--- pages/index.js
+++ pages/index.js
@@ -31,28 +31,37 @@ export default () => (
 const Anchor = styled.a`
   text-decoration: none;
   text-shadow: 2px 4px 3px rgba(0, 0, 0, 0.3);
-  &:hover {
-    color: cyan;
+  font-size: 1.5rem;
+  transition: all 100ms linear;
+
+  :hover {
+    color: gold;
   }
 `;
 
 const SayMyName = styled.span`
-  font-size: 30px;
+  font-size: 3rem;
   font-weight: bold;
   text-shadow: 0px 4px 3px rgba(0, 0, 0, 0.4), 0px 8px 13px rgba(0, 0, 0, 0.1),
     0px 18px 23px rgba(0, 0, 0, 0.1);
-  &:hover {
+  transition: all 100ms linear;
+
+  :hover {
     color: gold;
   }
 `;
 
 const Table = styled.table`
-  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
   font-size: 20px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 `;
 
 const Cell = styled.td`
