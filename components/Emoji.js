05f66e96e9c0f1050245e5af38fc07518c146f40
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-09-28 14:37:22 +0100

    removed emoji component

--- components/Emoji.js
+++
@@ -1,9 +0,0 @@
-export default props => (
-  <span
-    role="img"
-    aria-label={props.label ? props.label : ''}
-    aria-hidden={props.label ? 'false' : 'true'}
-  >
-    {props.symbol}
-  </span>
-);
