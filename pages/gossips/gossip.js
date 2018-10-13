287ddd48752e7dd7ec52575c0b8c52f3e1af6b09
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-10-13 18:20:27 +0100

    tweaked the gossip template a little bit

--- pages/gossips/gossip.js
+++ pages/gossips/gossip.js
@@ -1,10 +1,11 @@
 import Layout from '../../components/Layout';
 import butter from '../../dist/butterToken';
+import styled from 'styled-components';
 
 const Gossip = ({ gossip: { title, body } }) => (
   <Layout>
     <h1>{title}</h1>
-    <div dangerouslySetInnerHTML={{ __html: body }} />
+    <Wrapper dangerouslySetInnerHTML={{ __html: body }} />
   </Layout>
 );
 
@@ -13,5 +14,24 @@ Gossip.getInitialProps = async ({ query }) => {
   const resp = await butter.post.retrieve(slug);
   return { gossip: resp.data.data };
 };
+
+const Wrapper = styled.div`
+  font-size: 18px;
+
+  img {
+    box-shadow: 2px 2px 1px rgba(0, 0, 0, 0.3);
+
+    :hover {
+      opacity: 0.8;
+    }
+  }
+
+  figcaption {
+    text-align: center;
+    margin-top: 0.5em;
+    font-size: 14px;
+    font-style: italic;
+  }
+`;
 
 export default Gossip;
